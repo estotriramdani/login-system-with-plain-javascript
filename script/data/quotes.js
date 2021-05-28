9740aa74930ee6fc4989c1b8119e6175b18e3bd4
Esto Triramdani N <estolagi@gmail.com>
2021-05-28 21:36:13 +0700

    create skeleton

---
+++ script/data/quotes.js
@@ -0,0 +1,51 @@
+const quotes = [
+  {
+    quotes:
+      'Genius is one percent inspiration and ninety-nine percent perspiration.',
+    author: 'Thomas Edison',
+  },
+  {
+    quotes: "Talk doesn't cook rice",
+    author: 'Chinese Proverb',
+  },
+  {
+    quotes: 'One faces the future with ones past.',
+    author: 'Pearl Buck',
+  },
+  {
+    quotes: 'He who is contented is rich.',
+    author: 'Lao Tzu',
+  },
+  {
+    quotes: 'I destroy my enemies when I make them my friends. ',
+    author: 'Abraham Lincoln',
+  },
+  {
+    quotes: 'Let the beauty of what you love be what you do.',
+    author: 'Rumi',
+  },
+  {
+    quotes:
+      'The world turns aside to let any man pass who knows where he is going.',
+    author: 'Epictetus',
+  },
+  {
+    quotes:
+      'We are what we repeatedly do. Excellence, then, is not an act but a habit. ',
+    author: 'Aristotle',
+  },
+  {
+    quotes: 'We know the truth, not only by the reason, but by the heart.',
+    author: 'Blaise Pascal',
+  },
+  {
+    quotes: 'We choose our joys and sorrows long before we experience them.',
+    author: 'Kahlil Gibran',
+  },
+  {
+    quotes: 'Anybody can make history. Only a great man can write it. ',
+    author: 'Oscar Wilde',
+  },
+];
+
+export default quotes;
